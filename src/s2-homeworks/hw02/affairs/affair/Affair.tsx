4ae48ae346eb867340e4384bb2ef282207731d1c
ola <oliga.krivoshapco@gmail.com>
2025-01-25 22:40:23 +0200

    hw2

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -1,46 +1,46 @@
 import React from 'react'
-import { AffairType } from '../../HW2'
 import s from './Affair.module.css'
 import s2 from '../Affairs.module.css'
+import {AffairType} from "../../HW2";
+// import {AffairType} from "../../types";
 
 type AffairPropsType = {
-    // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: any // need to fix any
+    deleteAffairCallback: (_id: number) => void
 }
 
-function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {
-        // need to fix
-    }
+function Affair({
+                    affair,
+                    deleteAffairCallback
+                }: AffairPropsType) {
 
-    const nameClass = s.name + ' ' + s2[props.affair.priority]
-    const buttonClass = s.closeButton + ' ' + s2[props.affair.priority]
-    const affairClass = s.affair + ' ' + s2[props.affair.priority]
+    const deleteCallback = () => deleteAffairCallback(affair._id)
+
+// console.log(affair)
+    const nameClass = s.name + ' ' + s2[affair.priority]
+    const buttonClass = s.closeButton + ' ' + s2[affair.priority]
+    const affairClass = s.affair + ' ' + s2[affair.priority]
 
     return (
         <div
-            id={'hw2-affair-' + props.affair._id}
+            id={'hw2-affair-' + affair._id}
             className={affairClass}
         >
-            <div id={'hw2-name-' + props.affair._id} className={nameClass}>
-                {/*создаёт студент*/}
+            <div id={'hw2-name-' + affair._id} className={nameClass}>
+                {affair.name}
 
                 {/**/}
             </div>
-            <div id={'hw2-priority-' + props.affair._id} hidden>
-                {props.affair.priority}
+            <div id={'hw2-priority-' + affair._id} hidden>
+                {affair.priority}
             </div>
 
             <button
-                id={'hw2-button-delete-' + props.affair._id}
+                id={'hw2-button-delete-' + affair._id}
                 className={buttonClass}
-                // need to fix
+                onClick={deleteCallback}
 
-            >
-                {/*текст кнопки могут изменить студенты*/}
-                X
-                {/**/}
+            >X
             </button>
         </div>
     )
